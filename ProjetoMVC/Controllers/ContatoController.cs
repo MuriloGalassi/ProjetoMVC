e82d283e9068afdf44c6d9313dc859ee65fa9c4d
MuriloGalassi <murilo.galassi@hotmail.com>
2025-01-02 16:10:33 -0300

    Atualizando Nomenclatura

--- ProjetoMVC/Controllers/ContatoController.cs
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -50,8 +50,6 @@ namespace ProjetoMVC.Controllers
         }
 
         // POST: Contato/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,Telefone,Ativo")] Contato contato)
@@ -82,8 +80,7 @@ namespace ProjetoMVC.Controllers
         }
 
         // POST: Contato/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Telefone,Ativo")] Contato contato)
